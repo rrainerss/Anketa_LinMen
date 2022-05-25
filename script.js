3534567eeb5ab2866bf5c2340e2ae6f6850e49e4
rrainerss <rainersstrads8@gmail.com>
2022-05-25 17:35:23 +0300

    Initial files from Klāv

---
+++ script.js
@@ -0,0 +1,117 @@
+
+
+
+
+  function check1() {
+        var i = document.getElementsByClassName('Hidden2')[0].style.display = "none"; document.getElementById("41").checked = false; document.getElementsByClassName('Hidden5')[0].style.display = "none";
+        var o = document.getElementById("44").checked = false; document.getElementById("42").checked = false; document.getElementById("45").checked = false; document.getElementById("46").checked = false; document.getElementById("47").checked = false; document.getElementById("48").checked = false; document.getElementById("49").checked = false; document.getElementById("50").checked = false;
+
+      if (document.getElementById('3').checked && document.getElementById('4').checked && document.getElementById('5').checked) {
+          document.getElementsByClassName('Hidden')[0].style.display = "none";
+          document.getElementsByClassName('Hidden3')[0].style.display = "none";
+          document.getElementsByClassName('Hidden4')[0].style.display = "none";
+          i
+          o
+      } else {
+          document.getElementsByClassName('Hidden')[0].style.display = "block";
+          document.getElementsByClassName('Hidden3')[0].style.display = "none";
+          document.getElementsByClassName('Hidden4')[0].style.display = "none";
+      }
+    }
+  
+    function check2() {
+        var i = document.getElementsByClassName('Hidden2')[0].style.display = "none"; document.getElementById("41").checked = false; document.getElementsByClassName('Hidden5')[0].style.display = "none";
+        var o = document.getElementById("44").checked = false; document.getElementById("42").checked = false; document.getElementById("45").checked = false; document.getElementById("46").checked = false; document.getElementById("47").checked = false; document.getElementById("48").checked = false; document.getElementById("49").checked = false; document.getElementById("50").checked = false;
+        
+      if (document.getElementById('3').checked && document.getElementById('4').checked && document.getElementById('5').checked) {
+          document.getElementsByClassName('Hidden')[0].style.display = "none";
+          document.getElementsByClassName('Hidden3')[0].style.display = "none";
+          document.getElementsByClassName('Hidden4')[0].style.display = "none";
+          i
+          o
+      } 
+      
+      else {
+          document.getElementsByClassName('Hidden')[0].style.display = "none";
+          document.getElementsByClassName('Hidden3')[0].style.display = "block";
+          document.getElementsByClassName('Hidden4')[0].style.display = "none";
+          i
+          o
+      }
+    }
+  
+    function check3() {
+        var i = document.getElementsByClassName('Hidden2')[0].style.display = "none"; document.getElementById("41").checked = false; document.getElementsByClassName('Hidden5')[0].style.display = "none";
+        var o = document.getElementById("44").checked = false; document.getElementById("42").checked = false; document.getElementById("45").checked = false; document.getElementById("46").checked = false; document.getElementById("47").checked = false; document.getElementById("48").checked = false; document.getElementById("49").checked = false; document.getElementById("50").checked = false;
+      if (document.getElementById('3').checked && document.getElementById('4').checked && document.getElementById('5').checked) {
+          document.getElementsByClassName('Hidden')[0].style.display = "none";
+          document.getElementsByClassName('Hidden3')[0].style.display = "none";
+          document.getElementsByClassName('Hidden4')[0].style.display = "none";
+          i
+          o
+      } else {
+          document.getElementsByClassName('Hidden')[0].style.display = "none";
+          document.getElementsByClassName('Hidden3')[0].style.display = "none";
+          document.getElementsByClassName('Hidden4')[0].style.display = "block";
+          i
+          o
+      }
+    }
+
+    
+
+
+
+    function check4() {
+        var x = document.getElementById("43").checked = false; document.getElementById("44").checked = false;
+        if (document.getElementById('41').checked) {
+            document.getElementsByClassName('Hidden2')[0].style.display = "none";
+            x
+        } else {
+            document.getElementsByClassName('Hidden2')[0].style.display = "block";
+        }
+      }
+    
+      function check5() {
+        var x = document.getElementById("43").checked = false; document.getElementById("44").checked = false;
+        if (document.getElementById('41').checked) {
+            document.getElementsByClassName('Hidden2')[0].style.display = "none";
+            x
+        } else {
+            document.getElementsByClassName('Hidden2')[0].style.display = "none";
+            x
+        }
+      }
+
+
+
+
+      function check11() {
+        var x = document.getElementById("47").checked = false; document.getElementById("44").checked = false;
+        if (document.getElementById('48').checked) {
+            document.getElementsByClassName('Hidden5')[0].style.display = "none";
+            x
+        } else {
+            document.getElementsByClassName('Hidden5')[0].style.display = "block";
+        }
+      }
+
+      function check12() {
+        var x = document.getElementById("49").checked = false; document.getElementById("50").checked = false;
+        if (document.getElementById('48').checked) {
+            document.getElementsByClassName('Hidden5')[0].style.display = "none";
+            x
+        } else {
+            document.getElementsByClassName('Hidden5')[0].style.display = "none";
+            x
+        }
+      }
+
+
+
+// sekcija 2
+
+
+
+
+
