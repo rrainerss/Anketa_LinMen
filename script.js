fcf4ca4fc082ecb9e13d2b93eb8352961dc55f20
rrainerss <rainersstrads8@gmail.com>
2022-05-26 12:07:37 +0300

    polished some design elements and layout

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-
+$("input:checkbox").on('click', function()
+{
+    var $box = $(this);
+    if ($box.is(":checked"))
+    {
+        var group = "input:checkbox[name='" + $box.attr("name") + "']";
+        $(group).prop("checked", false);
+        $box.prop("checked", true);
+    }
+    else
+    {
+        $box.prop("checked", false);
+    }
+});
 
 
 
