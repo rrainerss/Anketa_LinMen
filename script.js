e235ca69282e15539bfc52ea74410b51a90b408b
rrainerss <rainersstrads8@gmail.com>
2022-05-27 11:39:58 +0300

    creating epic javascript

--- script.js
+++ script.js
@@ -1,32 +1,79 @@
-$("input:checkbox").on('click', function()
+//Allows inputs to work as checkboxes
+$(".FormSection input:checkbox").on('click', function()
 {
     var $box = $(this);
     if($box.is(":checked"))
     {
-        var group = "input:checkbox[name='" + $box.attr("name") + "']";
+        var group = ".FormSection input:checkbox[name='" + $box.attr("name") + "']";
         $(group).prop("checked", false);
+        $(group).attr("data-checked", false);
         $box.prop("checked", true);
+        $box.attr("data-checked", true);
     }
     else
     {
         $box.prop("checked", false);
+        $box.attr("data-checked", false);
     }
 });
+
+
+//Checkbox input logic
+function Section2(Group, Option, Target)
+{
+    //check if all 3 variables are passed
+    if((Group || Group === 0) && (Option || Option === 0) && (Target || Target === 0))
+    {
+        //check if clicked checkbox was already checked
+        if($("#Group" + Group + "_" + Option).attr('data-checked') == 'false')
+        {
+            //wasnt checked
+            console.log($("[id^='Group" + Group + "']"));
+
+            $("[id^='Group" + Group + "']").each(function(i, InputElement)
+            {
+                console.log(InputElement);
+            });
+        }
+        else
+        {
+            //was checked
+
+        }
+
+        //document.getElementById("Group" + Group + "_" + Option).checked = false;
+
+
+    }
+    else
+    {
+        alert("one of the variables are undefined");
+    }
+
+}
+
+
 
 
 
 function check1()
 {
-    var i = document.getElementsByClassName('Hidden2')[0].style.display = "none"; document.getElementById("41").checked = false; document.getElementsByClassName('Hidden5')[0].style.display = "none";
-    var o = document.getElementById("44").checked = false; document.getElementById("42").checked = false; document.getElementById("45").checked = false; document.getElementById("46").checked = false; document.getElementById("47").checked = false; document.getElementById("48").checked = false; document.getElementById("49").checked = false; document.getElementById("50").checked = false;
+    var i = document.getElementsByClassName('Hidden2')[0].style.display = "none";
+    document.getElementById("41").checked = false; document.getElementsByClassName('Hidden5')[0].style.display = "none";
+    var o = document.getElementById("44").checked = false;
+    document.getElementById("42").checked = false;
+    document.getElementById("45").checked = false;
+    document.getElementById("46").checked = false;
+    document.getElementById("47").checked = false;
+    document.getElementById("48").checked = false;
+    document.getElementById("49").checked = false;
+    document.getElementById("50").checked = false;
 
     if (document.getElementById('3').checked && document.getElementById('4').checked && document.getElementById('5').checked)
     {
         document.getElementsByClassName('Hidden')[0].style.display = "none";
         document.getElementsByClassName('Hidden3')[0].style.display = "none";
         document.getElementsByClassName('Hidden4')[0].style.display = "none";
-        i
-        o
     }
     else
     {
