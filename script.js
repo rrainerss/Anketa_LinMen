c9c27841871876a1c94741cb2019ff9794d51150
rrainerss <rainersstrads8@gmail.com>
2022-06-01 13:00:40 +0300

    revised js, almost done buttons

--- script.js
+++ script.js
@@ -30,18 +30,40 @@
     });
 
 //////////////////////////////
-//Checkbox input logic
-    function InputLogic(Group, Option, Target)
+//Descendant check
+    function DescendantCheck(Group)
     {
-        console.log(Group, Option, Target);
+        $("[id^='Group" + Group + "_']").each(function(element)
+        {
+            if($(this).attr("data-target"))
+            {
+                var SelectedTarget = document.getElementsByClassName($(this).attr("data-target"))[0];
+                SelectedTarget.style.display = "none";
+                
+                if(SelectedTarget.querySelector("input").hasAttribute("data-target"))
+                {
+                    console.log(SelectedTarget.querySelector("input"));
+                    DescendantOfTarget = SelectedTarget.querySelector("input").getAttribute("data-target");
+                    document.querySelector("." + DescendantOfTarget).style.display = "none";
+                }
+            }
+        });
+    }
 
-        //check if all 3 variables are passed
+//////////////////////////////
+//Checkbox input logic
+    function InputLogic2(Group, Option, Target)
+    {
+        //console.log("Group, option, target: " + Group, Option, Target);
+
+        //check if all 3 variables are passed (checkbox has descendants)
         if((Group || Group === 0) && (Option || Option === 0) && Target)
         {
             //check if clicked checkbox was already checked
+            //wasnt checked, then if runs
             if($("#Group" + Group + "_" + Option).attr("data-checked") == 'false')
             {
-                //wasnt checked
+                
                 $("[id^='Group" + Group + "']").each(function()
                 {
                     var GroupTargets = $(this).attr("data-target");
@@ -52,18 +74,10 @@
                     }
                 });
             }
+            //was checked, then else runs
             else
-            {
-                //was checked
-                $("[id^='Group" + Group + "']").each(function()
-                {
-                    console.log(this);
-                    if($(this).attr("data-target"))
-                    {
-                        var GroupTargets = $(this).attr("data-target");
-                        document.getElementsByClassName(GroupTargets)[0].style.display = "none";
-                    }
-                });
+            {             
+                DescendantCheck(Group);
             }
         }
         //not all 3 variables are passed, checkbox doesnt have descendants
@@ -78,4 +92,64 @@
                 }
             });
         }
+    }
+
+
+    function InputLogic(ClickedElement)
+    {        
+        var ClickedInput = ClickedElement.parent().prev();
+
+        //console.log(ClickedElement);
+
+        //if This WAS NOT CHECKED
+        if(ClickedInput.is(":checked") == false)
+        {
+            //gets attribute
+            var InputAttr = ClickedInput.attr("data-desc");
+            
+            //counts how many numbers it contains (determines level inside the button tree)
+            var AttrNumCount = InputAttr.replace(/[^0-9]/g, '').length;
+    
+            //matches all elements that start with same attribute (all should be descendants or parents of clicked checkbox)
+            $("[data-desc^='" + InputAttr + "']").each(function(i, Descendant)
+            {
+                //gets attribute of each element
+                DescAttr = Descendant.getAttribute("data-desc");
+
+                //counts how many numbers it contains
+                DescAttrNumCount = DescAttr.replace(/[^0-9]/g, '').length;
+
+                if(DescAttrNumCount == (AttrNumCount + 1))
+                {
+                    $("[data-desc='" + DescAttr + "']").each(function(i, MatchingElement)
+                    {
+                        MatchingElement.parentNode.style.display = "block";
+                    });
+                }
+            });
+
+        }
+
+        //if This WAS CHECKED
+        else if(ClickedInput.is(":checked") == true)
+        {
+            //gets attribute
+            var InputAttr = ClickedInput.attr("data-desc");
+            
+            //counts how many numbers it contains (determines level inside the button tree)
+            var AttrNumCount = InputAttr.replace(/[^0-9]/g, '').length;
+    
+            //matches all elements that start with same attribute (all should be descendants or parents of clicked checkbox)
+            $("[data-desc^='" + InputAttr + "']").each(function(i, Descendant)
+            {
+                if(InputAttr != Descendant.getAttribute("data-desc"))
+                {
+                    Descendant.parentNode.style.display = "none";
+                    Descendant.setAttribute("data-checked", false);
+                    Descendant.checked = false;
+                }
+            });
+        }
+
+
     }
